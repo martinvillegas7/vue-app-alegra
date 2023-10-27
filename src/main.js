217b6d5de9527906e6b647b51c1b79032121e3d1
jpcortesg <jpcortesg@eafit.edu.co>
2023-10-27 11:21:25 -0500

    Responsive Navbar

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { CoHamburgerMenu } from 'oh-vue-icons/icons'
+
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+addIcons(CoHamburgerMenu)
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .component('v-icon', OhVueIcon)
+  .mount('#app')
